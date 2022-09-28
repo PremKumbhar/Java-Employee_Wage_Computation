78c428783098bb60c7087449bc7e86eca92c5188
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-28 14:20:17 +0530

    [Prem]:Added switch case statement

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,30 +1,33 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+	 static int wagePerHr = 20;
+	    static int fullTimeHrs = 8;
+	    static int partTimeHrs = 4;
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Employee Wage Computation Program");
 
-	static int wagePerHr = 20;
-    static int fullTimeHrs = 8;
-    static int partTimeHrs = 4;
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+	        /*
+	        UC1
+	        check employee present or absent
+	         */
+	        int isPresent = (int) Math.floor(Math.random()*10)%3;
 
-        /*
-        UC1
-        check employee present or absent
-         */
-        int isPresent = (int) Math.floor(Math.random()*10)%3;
+	        /*
+	        * UC4
+	        * Solving using switch case
+	        * */
+	        switch (isPresent){
+	            case 1:
+	                System.out.println("Employee wage : "+ wagePerHr*fullTimeHrs);
+	                break;
+	            case 2:
+	                System.out.println("Employee wage : "+ wagePerHr*partTimeHrs);
+	                break;
+	            default:
+	                System.out.println("Employee wage : "+ 0);
+	                
+	        }
 
-        /*
-        * UC3
-        * Calculate daily wage with part time
-        * */
-        if(isPresent == 1){
-            System.out.println("Daily wage : "+ wagePerHr * fullTimeHrs);
-        }else if(isPresent == 2){
-            System.out.println("Daily wage : "+ wagePerHr * partTimeHrs);
-        }else{
-            System.out.println("Daily wage : " + 0 );
-        }
-
-    }
+	    }
 }
