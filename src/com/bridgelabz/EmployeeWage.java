bf51edb1713a22b9e4d2f0cd5139163fc70d62ce
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-28 14:16:54 +0530

    [Prem]:Added part time employee wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,27 +2,29 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-	 static int wagePerHr = 20;
-	    static int fullTimeHrs = 8;
-	    public static void main(String[] args) {
-	        System.out.println("Welcome to Employee Wage Computation Program");
+	static int wagePerHr = 20;
+    static int fullTimeHrs = 8;
+    static int partTimeHrs = 4;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
 
-	        /*
-	        UC1
-	        check employee present or absent
-	         */
-	        int isPresent = (int) Math.floor(Math.random()*10)%2;
+        /*
+        UC1
+        check employee present or absent
+         */
+        int isPresent = (int) Math.floor(Math.random()*10)%3;
 
-	        /*
-	        * UC2
-	        * Calculate daily wage
-	        * */
-	        if(isPresent == 1){
-	            System.out.println("Daily wage : "+ wagePerHr * fullTimeHrs);
-	        }else{
-	            System.out.println("Daily wage : " + 0 );
-	        }
+        /*
+        * UC3
+        * Calculate daily wage with part time
+        * */
+        if(isPresent == 1){
+            System.out.println("Daily wage : "+ wagePerHr * fullTimeHrs);
+        }else if(isPresent == 2){
+            System.out.println("Daily wage : "+ wagePerHr * partTimeHrs);
+        }else{
+            System.out.println("Daily wage : " + 0 );
+        }
 
-	    }
-
+    }
 }
