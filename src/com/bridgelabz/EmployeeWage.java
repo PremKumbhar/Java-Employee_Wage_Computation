8204f173ba4ed5c0bb79f9b532df907d9758b037
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-28 14:13:49 +0530

    [Prem]:Added calculate daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,19 +2,27 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-	public static void main(String[] args) {
-		        System.out.println("Welcome to Employee Wage Computation Program");
+	 static int wagePerHr = 20;
+	    static int fullTimeHrs = 8;
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Employee Wage Computation Program");
 
-		        /*
-		        UC1
-		        check employee present or absent
-		         */
-		        int isPresent = (int) Math.floor(Math.random()*10)%2;
-		        if(isPresent == 1){
-		            System.out.println("Employee is present");
-		        }else{
-		            System.out.println("Employee is absent");
-		        }
-	}
+	        /*
+	        UC1
+	        check employee present or absent
+	         */
+	        int isPresent = (int) Math.floor(Math.random()*10)%2;
+
+	        /*
+	        * UC2
+	        * Calculate daily wage
+	        * */
+	        if(isPresent == 1){
+	            System.out.println("Daily wage : "+ wagePerHr * fullTimeHrs);
+	        }else{
+	            System.out.println("Daily wage : " + 0 );
+	        }
+
+	    }
 
 }
