f1799adb7801b2bc00df6e4a5c659112b32013ba
PremKumbhar <kumbharprem7738@gmail.com>
2022-10-17 19:23:46 +0530

    [Prem]:Added save the total wage for each company file

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,41 +1,54 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-	//static int wagePerHr = 20;
-    static int fullTimeHrs = 8;
-    static int partTimeHrs = 4;
-  //  static int totalWorkingDays = 20;
-  //  static int totalWorkingHrs = 100;
+	 private String companyName;
+     private int wagePerHr;
+     private int totalWorkingDays;
+     private int totalWorkingHrs;
 
-    static void computeEmployeeWage(String company, int wagePerHr, int totalWorkingDays, int totalWorkingHrs){
-        int totalHrs = 0;
-        int days = 0;
-        while (totalHrs < totalWorkingHrs && days < totalWorkingDays){
-            days++;
+     
 
-            int isPresent = (int) Math.floor(Math.random()*10)%3;
-            switch (isPresent){
-                case 1:
-                    totalHrs += fullTimeHrs;
-                    break;
-                case 2:
-                    totalHrs += partTimeHrs;
-                    break;
-                default:
-                    totalHrs += 0;
+     public EmployeeWage(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
+		this.companyName = companyName;
+		this.wagePerHr = wagePerHr;
+		this.totalWorkingDays = totalWorkingDays;
+		this.totalWorkingHrs = totalWorkingHrs;
+	}
 
-            }
-        }
-        System.out.println("Monthly wage for : " + company + " is " +totalHrs * wagePerHr);
-    }
+
+	public int computeEmployeeWage(){
+     int fullTimeHrs = 8;
+     int partTimeHrs = 4;
+     int totalHrs = 0;
+     int days = 0;
+
+     while (totalHrs < totalWorkingHrs && days < totalWorkingDays){
+         days++;
+
+         int isPresent = (int) Math.floor(Math.random()*10)%3;
+         switch (isPresent){
+             case 1:
+                 totalHrs += fullTimeHrs;
+                 break;
+             case 2:
+                 totalHrs += partTimeHrs;
+                 break;
+             default:
+                 totalHrs += 0;
+
+         }
+     }
+     return totalHrs*wagePerHr;
+ }
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        EmployeeWage c1 = new EmployeeWage();
-        c1.computeEmployeeWage("TCS",25, 20, 100);
-        System.out.println("******************************************");
-        c1.computeEmployeeWage("Infosys", 20, 25, 120);
+        EmployeeWage c1 = new EmployeeWage("TCS",25, 20, 100);
+        System.out.println(c1.companyName+" : "+c1.computeEmployeeWage());
+        EmployeeWage c2 = new EmployeeWage("Infosys",20, 25, 120);
+        System.out.println(c2.companyName+" : "+c2.computeEmployeeWage());
         
         
 
