9dcac69a6ad2dc3f9047abeb8ec9ea15883a21be
PremKumbhar <kumbharprem7738@gmail.com>
2022-10-25 11:56:43 +0530

    [Prem]:Added manage employee wage of multiple companies using interface

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
-public class EmployeeWage {
-	 // Declaring a constant
+public class EmployeeWage implements IEmployeeWage {
+	// Declaring a constant
     public static final int IS_FULLTIME = 1;
     public static final int IS_PARTTIME = 2;
     private int numOfCompany = 0;
@@ -20,23 +20,10 @@ public class EmployeeWage {
 
 
     public int computeEmployeeWage(int wagePerHr, int totalWorkingDays, int totalWorkingHrs){
-        private String companyName;
-        private int wagePerHr;
-        private int totalWorkingDays;
-        private int totalWorkingHrs;
-
-        public EmployeeWage(String companyName,int wagePerHr, int totalWorkingDays, int totalWorkingHrs){
-            this.companyName = companyName;
-            this.wagePerHr = wagePerHr;
-            this.totalWorkingDays = totalWorkingDays;
-            this.totalWorkingHrs = totalWorkingHrs;
-        }
-
-        public int computeEmployeeWage(){
-            int fullTimeHrs = 8;
-            int partTimeHrs = 4;
-            int totalHrs = 0;
-            int days = 0;
+        int fullTimeHrs = 8;
+        int partTimeHrs = 4;
+        int totalHrs = 0;
+        int days = 0;
             while (totalHrs < totalWorkingHrs && days < totalWorkingDays){
                 days++;
                 int isPresent = (int) Math.floor(Math.random()*10)%3;
@@ -51,29 +38,11 @@ public class EmployeeWage {
                         totalHrs += 0;
                 }
             }
-
-        while (totalHrs < totalWorkingHrs && days < totalWorkingDays){
-            days++;
-
-            int isPresent = (int) Math.floor(Math.random()*10)%3;
-            switch (isPresent){
-                case 1:
-                    totalHrs += fullTimeHrs;
-                    break;
-                case 2:
-                    totalHrs += partTimeHrs;
-                    break;
-                default:
-                    totalHrs += 0;
-
-            }
-        }
         return totalHrs*wagePerHr;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-
 
         EmployeeWage employeeWage = new EmployeeWage();
         employeeWage.addCompany("TCS",20,20,100);
@@ -84,6 +53,7 @@ public class EmployeeWage {
         for (int i = 0; i < employeeWage.numOfCompany; i++){
             System.out.println(employeeWage.companyEmpWages[i].getCompanyName() + " : "+ employeeWage.companyEmpWages[i].getTotalEmpWage());
         }
+
 
     }
  }
---
+++ src/com/bridgelabz/IEmployeeWage.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+
+public interface IEmployeeWage {
+	public void addCompany(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs);
+    public int computeEmployeeWage(int wagePerHr, int totalWorkingDays, int totalWorkingHrs);
+}
